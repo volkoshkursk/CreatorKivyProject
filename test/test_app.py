110860015685b45f00f56bb15587b662b260f9b9
HeaTTheatR <gorodage@gmail.com>
2018-02-17 21:43:09 +0200

    View current directory

--- test/test_app.py
+++ test/test_app.py
@@ -2,12 +2,15 @@
 This file is responsible for testing Apps inside the CreatorKivyProject project.
 '''
 
+import os
+import sys
 import unittest
 
 
 class AppTest(unittest.TestCase):
 
     def test_creator_app(self):
+        print(os.path.split(os.path.abspath(sys.argv[0]))[0])
         from test import Test
         test = Test()
         test.run()
