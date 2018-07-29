de81d09993cd193b394a55a03abebf32bd93fb51
HeaTTheatR <gorodage@gmail.com>
2018-07-29 11:51:02 +0300

    Add support Python 2.7

--- libs/utils/showplugins.py
+++ libs/utils/showplugins.py
@@ -29,7 +29,9 @@ class ShowPlugins(object):
     def _save_status_plugin(self, dialog, name_plugin, result):
         if result == self._app.translation._('Да'):
             self._list_activate_plugins.append(name_plugin)
-            with open(os.path.join(self._app.directory, 'libs', 'plugins', 'plugins_list.list'), 'w') as plugin_list:
+            with open(os.path.join(
+                    self._app.directory, 'libs', 'plugins', 'plugins_list.list'
+            ), 'w') as plugin_list:
                 plugin_list.write(str(self._list_activate_plugins))
         else:
             for item_list in self._list_plugins.ids.md_list.children:
@@ -51,7 +53,10 @@ class ShowPlugins(object):
             else:
                 try:
                     self._list_activate_plugins.remove(name_plugin)
-                    with open(os.path.join(self._app.directory, 'libs', 'plugins', 'plugins_list.list'), 'w') as plugin_list:
+                    with open(
+                            os.path.join(
+                                self._app.directory, 'libs', 'plugins',
+                                'plugins_list.list'), 'w') as plugin_list:
                         plugin_list.write(str(self._list_activate_plugins))
                 except ValueError:
                     pass
@@ -64,11 +69,16 @@ class ShowPlugins(object):
 
         dict_info_plugins = {}
         self._list_activate_plugins = literal_eval(
-            open(os.path.join(self._app.directory, 'libs', 'plugins', 'plugins_list.list'), encoding='utf-8').read()
+            open(os.path.join(
+                self._app.directory, 'libs', 'plugins', 'plugins_list.list'),
+                encoding='utf-8').read()
         )
 
-        for plugin in os.listdir(os.path.join(self._app.directory, 'libs', 'plugins')):
-            if not os.path.isdir(os.path.join(self._app.directory, 'libs', f'plugins/{plugin}')):
+        for plugin in os.listdir(
+                os.path.join(self._app.directory, 'libs', 'plugins')):
+            if not os.path.isdir(
+                    os.path.join(
+                        self._app.directory, 'libs', 'plugins/%s' % plugin)):
                 continue
 
             if plugin in self._list_activate_plugins:
@@ -79,7 +89,8 @@ class ShowPlugins(object):
             plugin_desc = self._app.started_plugins[plugin]['plugin-desc']
             plugin_icon = os.path.join(
                 self._app.directory, 'libs', 'plugins',
-                self._app.started_plugins[plugin]['plugin-package'], 'plugin_logo.png'
+                self._app.started_plugins[plugin]['plugin-package'],
+                'plugin_logo.png'
             )
 
             if not os.path.exists(plugin_icon):
@@ -91,14 +102,19 @@ class ShowPlugins(object):
     def _show_info_plugin(self, name_plugin):
         '''Вызывается при клике на имя плагина из списка.'''
 
-        if not os.path.exists(os.path.join(self._app.directory, 'libs', 'plugins', name_plugin, 'README.rst')):
+        if not os.path.exists(
+                os.path.join(
+                    self._app.directory, 'libs', 'plugins', name_plugin,
+                    'README.rst')):
             dialog(
                 text=self._app.translation._('Нет информации о плагине!'),
                 title=name_plugin
             )
         else:
             info_plugin = \
-                open(os.path.join(self._app.directory, 'libs', 'plugins', name_plugin, 'README.rst'), encoding='utf-8').read()
+                open(os.path.join(
+                    self._app.directory, 'libs', 'plugins', name_plugin,
+                    'README.rst'), encoding='utf-8').read()
             info_plugin = info_plugin.format(
                 NAME_APP=self._app.title,
                 VERSION=self._app.started_plugins[name_plugin]['plugin-version'],
@@ -126,5 +142,5 @@ class ShowPlugins(object):
                 dict_items=dict_info_plugins,
                 events_callback=self._action_plugin, flag='two_list_icon_check'
             )
-            self.window_user_plugins = card(self._list_plugins, size=(.85, .55) )
+            self.window_user_plugins = card(self._list_plugins, size=(.85, .55))
         self.window_user_plugins.open()
